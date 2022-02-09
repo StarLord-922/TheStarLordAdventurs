d7a8e70ea536945d61869b5a439b4e2407733e75
Seddik Kadi <Seddik.tizit@gmail.com>
2022-02-09 15:35:22 +0100

    last version App.vue header

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
+    <router-link to="/test">Test</router-link>
+    <router-link to="/newpage">NewPage</router-link>
   </div>
   <router-view/>
 </template>
@@ -16,7 +18,7 @@
 }
 
 #nav {
-  padding: 30px;
+  padding: 70px;
 }
 
 #nav a {
